fc69f0728f29cd9ff17b1fa23f2e67c7bfe8c9cb
Shanaka Caldera <shanaka.vx@gmail.com>
2016-08-25 13:21:46 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+    BASE PATHS
+----------------
+
+Project name = LireFrontend
+
+Please put this project in your C:\wamp\www
+
+Absolute path to voice profiles = C:\wamp\www\LireFrontend\voiceprofiles
+Absolute path to uploading records = C:\wamp\www\LireFrontend\voiceuploads
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // this is the index page for dasboard timeline and tokenizer
+        //Recording will go in another pages
+        
+        ?>
+    </body>
+</html>
